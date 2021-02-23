9af8dc726e21200070b8490dc7fc798a42a91e13
dulguun <rockaveue@gmail.com>
2021-02-23 16:06:11 +0800

    dis-bott

--- cogs/music.py
+++ cogs/music.py
@@ -65,7 +65,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
         await self.bot.wavelink.initiate_node(
             host = 'dis-lavalink.herokuapp.com',#127.0.0.1
             port = '80',#7000
-            rest_uri = 'https://dis-lavalink.herokuapp.com:80',
+            rest_uri = 'dis-lavalink.herokuapp.com:80',
             password = 'hithere',
             identifier = 'TEST',
             region = 'hong_kong'
