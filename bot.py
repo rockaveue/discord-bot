c608e3d40e9cb9bfec9285ee4e0eb3baf508fd93
dulguun <rockaveue@gmail.com>
2021-04-04 22:17:09 +0800

    dis-bott

--- bot.py
+++ bot.py
@@ -356,10 +356,11 @@ async def unload(ctx, extension):
 async def reload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+    
+# load extension
+# for filename in os.listdir('./cogs'):
+#     if filename.endswith('.py'):
+#         bot.load_extension(f'cogs.{filename[:-3]}')
 
 # @bot.command(pass_context=True,aliases=['j'])
 # async def join(ctx):
